92af155d6a47dddc2ae9cfc75bc10b5afbb236f5
Mahesh Sunuwar <ms@MacBook.local>
2023-10-09 13:32:32 -0500

    clip projoct added

---
+++ clip/Readme.md
@@ -0,0 +1,11 @@
+## CLIP: Connecting Texts and Images
+
+### Contrastive Language-Image Pre-training (CLIP)
+### Contrastive Language-Image Pre-training (CLIP)
+
+This is the paper [CLIP: Connecting Text and Images](https://arxiv.org/abs/2103.00020) by Alec Radford, Jong Wook Kim, Chris Hallacy, Aditya Ramesh, Gabriel Goh, Sandhini Agarwal, Girish Sastry, Amanda Askell, Pamela Mishkin, Jack Clark, Gretchen Krueger, Ilya Sutskever.
+
+Official Website: [OpenAI - CLIP](https://openai.com/blog/clip/)
+
+This paper is about a new neural network model called CLIP, which is a neural network model that can be used for zero-shot learning.
+
---
+++ clip/run.py
@@ -0,0 +1,69 @@
+# %%
+# https://thecleverprogrammer.com/2021/12/13/alexnet-architecture-using-python/
+
+# %%
+import keras
+import tensorflow as tf
+import keras.layers as layers
+
+model = keras.Sequential()
+
+model.add(layers.Conv2D(
+    filters=96, kernel_size=(11, 11),
+    strides=(4, 4), input_shape=(227, 227, 3),
+    activation='relu'
+))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)))
+
+model.add(layers.Conv2D(
+    filters=256, kernel_size=(5, 5),
+    strides=(1, 1), activation='relu',
+    padding='same'
+))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)))
+
+model.add(layers.Conv2D(
+    filters=384, kernel_size=(5, 5),
+    strides=(1, 1), activation='relu'
+))
+model.add(layers.BatchNormalization())
+
+model.add(layers.Conv2D(
+    filters=384, kernel_size=(3, 3),
+    strides=(1, 1), activation='relu',
+    padding='same'
+))
+model.add(layers.BatchNormalization())
+
+model.add(layers.Conv2D(
+    filters=256, kernel_size=(3, 3),
+    strides=(1, 1), activation='relu',
+    padding='same'
+))
+model.add(layers.BatchNormalization())
+
+model.add(layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)))
+
+model.add(layers.Flatten())
+
+model.add(layers.Dense(4096, activation='relu'))
+model.add(layers.Dropout(0.5))
+
+model.add(layers.Dense(1000, activation='softmax'))
+
+# %%
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    optimizer=tf.optimizers.SGD(lr=0.001),
+    metrics=['accuracy']
+)
+model.summary()
+
+# %%
+import visualkeras
+import matplotlib.pyplot as plt
+
+plt.imshow(visualkeras.layered_view(model))
+plt.show()
